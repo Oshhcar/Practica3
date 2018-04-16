edc576c8192b213feb7a7e308a4a229c829a0d5d
Oscar M <oskralberto14@gmail.com>
2018-04-16 17:20:33 -0600

    Modificando la conexion - 201213336

--- Practica3/Practica3/Clases/Conexion.cs
+++ Practica3/Practica3/Clases/Conexion.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
 
@@ -13,5 +14,16 @@ public class Conexion
         //
         // TODO: Agregar aquí la lógica del constructor
         //
+    }
+
+    //Conexión a la base de datos
+    private static SqlConnection conexion = new SqlConnection
+        (
+        "Data Source=DESKTOP-DV9JSGM\\SQLEXPRESS; Initial Catalog=ANALISIS_P3; Integrated Security=True;"
+        );
+
+    public static SqlConnection RecibirConexion()
+    {
+        return conexion;
     }
 }
