16f4b1b3e1f5507ef71310f3aa824be862970048
Oscar M <oskralberto14@gmail.com>
2018-04-16 17:12:33 -0600

    Creación clases para el proyecto - 201213336

---
+++ Practica3/Practica3/Clases/SQL.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+/// <summary>
+/// Descripción breve de SQL
+/// </summary>
+public class SQL
+{
+    public SQL()
+    {
+        //
+        // TODO: Agregar aquí la lógica del constructor
+        //
+    }
+}
