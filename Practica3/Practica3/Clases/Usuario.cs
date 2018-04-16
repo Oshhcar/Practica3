9eb94a70088d42c834b7c36c859c4a303e92ca8b
Oscar M <oskralberto14@gmail.com>
2018-04-16 17:45:45 -0600

    Modificando usuario.cs - 201213336

--- Practica3/Practica3/Clases/Usuario.cs
+++ Practica3/Practica3/Clases/Usuario.cs
@@ -14,4 +14,35 @@ public class Usuario
         // TODO: Agregar aquí la lógica del constructor
         //
     }
+
+    public int Login(String cuenta, String usuario, String contraseña)
+    {
+        //1 = correcta
+        //0 = incorrecta
+        //2 = no existe nombre de usuario o cuenta. 
+        SQL mi_consulta = new SQL();
+
+
+        try
+        {
+            String contraseñaTomada = mi_consulta.verificarContraseña(cuenta);
+            if (contraseñaTomada.Equals(contraseña))
+            {
+                System.Diagnostics.Debug.Write("Nombre de usuario y contraseña correctos");
+                return 1;
+
+            }
+            else
+            {
+                System.Diagnostics.Debug.Write("Nombre de usuario y contraseña incorrectos");
+                return 0;
+            }
+
+        }
+        catch
+        {
+            System.Diagnostics.Debug.Write("Nombre de usuario no existe");
+            return 2;
+        }
+    }
 }
